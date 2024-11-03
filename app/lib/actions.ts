97a3ba379965bb2bb65c91ee53ad223cc6ff5529
uneezaismail <ismailrahet@gmail.com>
2024-11-03 19:06:05 +0500

    Update actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -21,7 +21,11 @@ export async function authenticate(
   formData: FormData,
 ) {
   try {
-    await signIn('credentials', formData);
+await signIn('credentials', {
+  redirectTo:'/dashboard',
+  email: formData.get('email'),
+  password : formData.get('password'),
+});
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
